c09fcd226ef9b7d112fde2df6aac6c155b1a3e34
vladyslavyatsun <angrydog47@gmail.com>
2017-04-29 16:48:39 +0300

    Add recursive karatsuba multiplication.

--- src/main/java/Karatsuba.java
+++ src/main/java/Karatsuba.java
@@ -4,7 +4,7 @@ import java.math.BigInteger;
  * Created by dog on 4/23/17.
  */
 public class Karatsuba {
-    public String karatsubaMultiplication(String x, String y) {
+    public String multiplication(String x, String y) {
         int xCapasity = x.length();
         int yCapasity = y.length();
 
@@ -23,4 +23,39 @@ public class Karatsuba {
         return result.toString();
     }
 
+    public String multiplicationRecursive(String x, String y) {
+        if (x.length() == 1 && y.length() == 1) {
+            return Byte.parseByte(x) * Byte.parseByte(y) + "";
+        }
+
+        int capasity = x.length() > y.length() ? x.length() : y.length();
+        if (capasity % 2 == 1) capasity++;
+
+        while (capasity != x.length())
+        {
+            x = "0"+x;
+        }
+
+        while (capasity != y.length())
+        {
+            y = "0"+y;
+        }
+
+        String a = x.substring(0, capasity / 2);
+        String b = x.substring(capasity / 2);
+
+        String c = y.substring(0, capasity / 2);
+        String d = y.substring(capasity /2);
+
+        BigInteger ac = new BigInteger(multiplicationRecursive(a, c));
+        BigInteger bd = new BigInteger(multiplicationRecursive(b, d));
+
+        BigInteger apb = new BigInteger(a).add(new BigInteger(b));
+        BigInteger cpd = new BigInteger(c).add(new BigInteger(d));
+
+        BigInteger z = new BigInteger(multiplicationRecursive(apb.toString(), cpd.toString())).subtract(ac).subtract(bd);
+
+        BigInteger result = ac.multiply(new BigInteger("10").pow(capasity)).add(z.multiply(new BigInteger("10").pow(capasity/2))).add(bd);
+        return result.toString();
+    }
 }
